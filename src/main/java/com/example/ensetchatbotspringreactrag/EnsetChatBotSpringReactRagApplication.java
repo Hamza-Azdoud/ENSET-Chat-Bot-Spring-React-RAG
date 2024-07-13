649704ddf64c565c04b3eda716b4dde8cd12baf9
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-07-13 16:09:40 +0100

    first commit

---
+++ src/main/java/com/example/ensetchatbotspringreactrag/EnsetChatBotSpringReactRagApplication.java
@@ -0,0 +1,13 @@
+package com.example.ensetchatbotspringreactrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsetChatBotSpringReactRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsetChatBotSpringReactRagApplication.class, args);
+    }
+
+}
