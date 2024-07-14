5b1ff3377cc89058b2aa0647f81a96664a6ccf05
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-07-14 21:21:45 +0100

    Creation de Store

--- src/main/java/com/example/ensetchatbotspringreactrag/services/ChatAiService.java
+++ src/main/java/com/example/ensetchatbotspringreactrag/services/ChatAiService.java
@@ -3,19 +3,26 @@ package com.example.ensetchatbotspringreactrag.services;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.stereotype.Service;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
+
+import java.util.List;
 
 @BrowserCallable
 @AnonymousAllowed
 public class ChatAiService {
 
     private ChatClient chatClient;
+    private VectorStore vectorStore;
 
-    public ChatAiService(ChatClient.Builder builder) {
+    public ChatAiService(ChatClient.Builder builder, VectorStore vectorStore) {
         this.chatClient = builder.build();
+        this.vectorStore = vectorStore;
     }
 
     public String ragChat(String question){
+        List<Document> documents = vectorStore.similaritySearch(question);
+        List<String> context = documents.stream().map(Document::getContent).toList();
         return chatClient.prompt()
                 .user(question)
                 .call()
